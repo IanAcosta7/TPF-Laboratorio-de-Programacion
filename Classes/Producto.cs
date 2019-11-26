2dff6c8e91b031d56648667b0141ea5120b872af
martingomezvega <tincho3670@gmail.com>
2019-11-26 16:58:19 -0300

    Revert "NO SIRVE"
    This reverts commit 440b10e8103f344e4335960548598cffbee147e2.

--- Classes/Producto.cs
+++ Classes/Producto.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Windows.Forms;
-using TPF_Laboratorio_de_Programacion.Forms.Ventas_Forms;
 
 namespace TPF_Laboratorio_de_Programacion
 {
@@ -19,7 +18,7 @@ namespace TPF_Laboratorio_de_Programacion
         public int stock;
         public double precio;
 
-        public Producto(int newID, string newNombre, string newMarca, string newColor, double newTalle, int newStock, double newPrecio)
+        public Producto (int newID, string newNombre, string newMarca, string newColor, double newTalle, int newStock, double newPrecio)
         {
             id_producto = newID;
             nombre = newNombre;
@@ -45,7 +44,7 @@ namespace TPF_Laboratorio_de_Programacion
             return ds.Tables[0];
         }
 
-        public static DataSet getProductByName(string name)
+        public static DataSet getProductByName (string name)
         {
             // Conexion BD
             string cmd = string.Format("SELECT * FROM Productos WHERE nombre='{0}'", name);
@@ -53,13 +52,12 @@ namespace TPF_Laboratorio_de_Programacion
 
             return ds;
         }
-
+        
         public static void borrarProducto(string nombre)
         {
             string cmd = string.Format("EXEC EliminarProducto '{0}'", nombre);
             Utilidades.Ejecutar(cmd);
         }
-
 
         public static Boolean validarFormulario(Control Objeto, ErrorProvider ErrorProvider)
         {
@@ -87,20 +85,17 @@ namespace TPF_Laboratorio_de_Programacion
                         {
                             ErrorProvider.SetError(Obj, "Debe completar todos los campos");
                             HayErrores = true;
-                        }
-                        else if (Obj.Name == "txtNombre" && validarNombre(Obj.Text, id)) //nombreExistente(Obj.Text) && !Obj.ValidarDoble) 
+                        } else if (Obj.Name == "txtNombre" && validarNombre(Obj.Text, id)) //nombreExistente(Obj.Text) && !Obj.ValidarDoble) 
                         // Validacion nombre
                         {
                             ErrorProvider.SetError(Obj, "El nombre ya existe");
                             HayErrores = true;
-                        }
-                        else if ((Obj.Name == "txtPrecio" || Obj.Name == "txtTalle") && !Double.TryParse(Obj.Text, out dbResult))
+                        } else if ((Obj.Name == "txtPrecio" || Obj.Name == "txtTalle") && !Double.TryParse(Obj.Text, out dbResult))
                         // Validacion talle y precio
                         {
                             ErrorProvider.SetError(Obj, "Solo se aceptan valores decimales");
                             HayErrores = true;
-                        }
-                        else if (Obj.Name == "txtStock" && !Int32.TryParse(Obj.Text, out iResult))
+                        } else if (Obj.Name == "txtStock" && !Int32.TryParse(Obj.Text, out iResult))
                         // Validación stock
                         {
                             ErrorProvider.SetError(Obj, "No es un número entero");
@@ -114,7 +109,7 @@ namespace TPF_Laboratorio_de_Programacion
                 }
             }
             return HayErrores;
-        }
+        } 
 
         public static Boolean validarNombre(string nombre, int id)
         {
@@ -136,7 +131,7 @@ namespace TPF_Laboratorio_de_Programacion
             return flag;
         }
 
-        public static Boolean nombreExistente(string nombre)
+        public static Boolean nombreExistente (string nombre)
         {
             Boolean existe = false;
 
@@ -149,41 +144,6 @@ namespace TPF_Laboratorio_de_Programacion
             }
 
             return existe;
-        }
-
-        public static Boolean validarCantidad(Control ObjetoP, ErrorProvider errorProvider, int cant)
-        {
-            Boolean HayError=false;
-
-            foreach (Control Item in ObjetoP.Controls)
-            {
-                if (Item is ErrorTextBox)
-                {
-                    ErrorTextBox Obj = (ErrorTextBox)Item;
-
-                    if (Obj.Validar == true)
-                    {
-                        if (string.IsNullOrEmpty(Obj.Text.Trim()))
-                        {
-                            errorProvider.SetError(Obj, "Falta ingresar una cantidad");
-                            HayError = true;
-                        }
-
-                        if (cant < 1)
-                        {
-                            errorProvider.SetError(Obj, "Se permiten solamanete valores mayores a cero");
-                            HayError = true;
-
-                        }
-
-                    }
-                    else
-                    {
-                        errorProvider.SetError(Obj, "");//// se puede sacar
-                    }
-                }
-            }
-            return HayError;
         }
     }
 }
